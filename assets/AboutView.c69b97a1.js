a34ec40fcc0ad44f70ea871c414bd184c60ade9d
metabynull <metabynull@users.noreply.github.com>
2022-07-28 04:48:13 +0000

    deploy: meta-secret/meta-secret-node@d930c858f2ab7ad4eb0c9bad082434f90d6abf1b

---
+++ assets/AboutView.c69b97a1.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,a as t}from"./index.6a424da6.js";const n={},c=t("h1",null,"This is an about page",-1),a=t("button",{type:"button"}," Options ",-1),_=[c,a];function r(i,u){return o(),s("div",null,_)}const l=e(n,[["render",r]]);export{l as default};
